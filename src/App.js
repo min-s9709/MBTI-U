55cc55661bc900255833b37acea951172ecab451
min-s9709 <78842816+min-s9709@users.noreply.github.com>
2023-03-29 23:20:21 +0900

    Page 라우팅 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Board from "./pages/Board";
+import BoardDetail from "./pages/BoardDetail";
+import Chat from "./pages/Chat";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Movie from "./pages/Movie";
+import MovieDetail from "./pages/MovieDetail";
+import Register from "./pages/Register";
+
 const App = () => {
-  <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<Movie />} />
+        <Route path="/movie/:id" element={<MovieDetail />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/board" element={<Board />} />
+        <Route path="/board/:id" element={<BoardDetail />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
