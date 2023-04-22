63e2a3d059b645ad3037e24c9ff9853590e80c63
min-s9709 <78842816+min-s9709@users.noreply.github.com>
2023-04-22 20:20:45 +0900

    게시글 상세보기 기초작업

--- src/pages/BoardDetail.js
+++ src/pages/BoardDetail.js
@@ -1,5 +1,38 @@
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+const BoardDetailWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  width: 600px;
+  height: 600px;
+`;
+
+const BoardDetailTitle = styled.div`
+  width: 320px;
+  height: 150px;
+  margin-bottom: 50px;
+  background-color: white;
+`;
+
+const BoardDetailContent = styled.div`
+  width: 320px;
+  height: 250px;
+  background-color: white;
+`;
+
 const BoardDetail = () => {
-  return <div>Board Detail Page</div>;
+  const { id } = useParams();
+  console.log(id);
+  return (
+    <BoardDetailWrapper>
+      <BoardDetailTitle></BoardDetailTitle>
+      <BoardDetailContent></BoardDetailContent>
+    </BoardDetailWrapper>
+  );
 };
 
 export default BoardDetail;
