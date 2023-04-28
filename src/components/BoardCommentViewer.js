d75d04c7fae034653eac0a73efcdfd79411aede7
min-s9709 <78842816+min-s9709@users.noreply.github.com>
2023-04-28 18:21:34 +0900

    댓글 조회 및 작성하기 api 연동
    게시물 최신순 내림차순
    게시물마다 댓글 페이지네이션

--- src/components/BoardCommentViewer.js
+++ src/components/BoardCommentViewer.js
@@ -3,40 +3,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCommentDots } from "@fortawesome/free-regular-svg-icons";
 import BoardCommentListItem from "./BoardCommentListItem";
 import { useParams } from "react-router-dom";
-
-const boardCommentList = [
-  {
-    id: 1,
-    regdate: "2023.04.27",
-    content: "test comment",
-    writer: "testID",
-  },
-  {
-    id: 2,
-    regdate: "2023.04.27",
-    content: "test comment2",
-    writer: "testID",
-  },
-  {
-    id: 3,
-    regdate: "2023.04.27",
-    content: "test comment3",
-    writer: "testID",
-  },
-  {
-    id: 4,
-    regdate: "2023.04.27",
-    content: "test comment4",
-    writer: "testID",
-  },
-  {
-    id: 5,
-    regdate: "2023.04.27",
-    content: "test comment5",
-    writer: "testID",
-  },
-];
-
+import { useState, useEffect } from "react";
+import { boardCommentPost, getBoardCommentList } from "../lib/api";
+import { userInfoState } from "../recoil/userAtom";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { boardCommentList } from "../recoil/boardComment";
+import Pagination from "react-js-pagination";
+import { PaginationBox } from "./BoardList";
 const CommentWrapper = styled.div`
   padding-left: 10px;
   padding-right: 10px;
@@ -96,6 +69,46 @@ const Button = styled.button`
 
 const BoardCommentViewer = () => {
   const { id } = useParams();
+  const userData = useRecoilValue(userInfoState);
+  const [boardComments, setBoardComments] = useRecoilState(boardCommentList);
+  const [commentContent, setCommentContent] = useState("");
+  const [page, setPage] = useState(1);
+  const items = 5;
+  const newCommentItem = {
+    commentContent,
+    regdate: new Date().toLocaleDateString(),
+    writer: userData.userNick,
+  };
+
+  const boardCommentsList = [...boardComments];
+  const sortedBoardComments = boardCommentsList.sort((a, b) => b.id - a.id);
+
+  const handlePageChange = (page) => {
+    setPage(page);
+  };
+
+  const handleCommentChange = (e) => {
+    setCommentContent(e.target.value);
+  };
+  const handleCommentPost = async () => {
+    try {
+      const result = await boardCommentPost(id, newCommentItem);
+      if (result.resultCode === "success") {
+        alert("댓글 작성 완료!");
+      }
+    } catch (e) {
+      alert("댓글 작성 실패");
+    }
+    setCommentContent("");
+  };
+  useEffect(() => {
+    const getCommentList = async () => {
+      const result = await getBoardCommentList(id);
+      setBoardComments(result);
+    };
+    getCommentList();
+  }, [id, setBoardComments, boardComments]);
+  console.log(boardComments);
   return (
     <CommentWrapper>
       <CommentHead>
@@ -103,12 +116,31 @@ const BoardCommentViewer = () => {
         <FontAwesomeIcon icon={faCommentDots} size="xl" />
       </CommentHead>
       <CommentPost>
-        <CommentContent placeholder="댓글을 작성해주세요."></CommentContent>
-        <Button>등록하기</Button>
+        <CommentContent
+          onChange={handleCommentChange}
+          value={commentContent}
+          placeholder="댓글을 작성해주세요."
+        ></CommentContent>
+        <Button onClick={handleCommentPost}>등록하기</Button>
       </CommentPost>
-      {boardCommentList.map((item) => (
-        <BoardCommentListItem key={item.id} {...item} />
-      ))}
+      {sortedBoardComments
+        ? sortedBoardComments
+            .slice(items * (page - 1), items * (page - 1) + items)
+            .map((item) => <BoardCommentListItem key={item.id} {...item} />)
+        : null}
+      <PaginationBox>
+        <Pagination
+          activePage={page}
+          itemsCountPerPage={items}
+          totalItemsCount={sortedBoardComments.length - 1}
+          pageRangeDisplayed={
+            sortedBoardComments.length % items === 0
+              ? sortedBoardComments.length / items
+              : sortedBoardComments.length / items + 1
+          }
+          onChange={handlePageChange}
+        />
+      </PaginationBox>
     </CommentWrapper>
   );
 };
