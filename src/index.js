8044fd56b1c62394c18f450567dc95052efcf63a
min-s9709 <78842816+min-s9709@users.noreply.github.com>
2023-05-05 19:06:39 +0900

    react-query 적용

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { GlobalStyle } from "./styles/GlobalStyle";
 import { RecoilRoot } from "recoil";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const client = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <RecoilRoot>
-    <GlobalStyle />
-    <App />
+    <QueryClientProvider client={client}>
+      <GlobalStyle />
+      <App />
+    </QueryClientProvider>
   </RecoilRoot>
 );
