f1e96b65fddfd1cba8b64cf56be995f42ce83b3a
min-s9709 <78842816+min-s9709@users.noreply.github.com>
2023-05-15 17:27:19 +0900

    Home 화면 변경

--- src/components/HomeViewer.js
+++ src/components/HomeViewer.js
@@ -1,13 +1,10 @@
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import { Link } from "react-router-dom";
 
 const Wrapper = styled.div`
   width: 1024px;
   margin: 0 auto;
-  margin-top: 150px;
+  margin-top: 120px;
   @media (max-width: 1024px) {
     width: 768px;
   }
@@ -15,29 +12,71 @@ const Wrapper = styled.div`
     width: 100%;
   }
 `;
-const SliderItem = styled.img`
-  width: 600px;
-  height: 400px;
-  border-radius: 15px;
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  width: 65vw;
+  gap: 2em;
+  div:first-child {
+    grid-column: span 2;
+  }
+  margin: 0 auto;
+`;
+
+const HomeBox = styled.div`
+  height: 200px;
+  background-color: rgba(255, 255, 255, 1);
+  margin-top: 10px;
+  margin-left: 10px;
+  border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.6);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  cursor: pointer;
+  &:hover {
+    scale: 1.1;
+  }
+  transition: scale, 0.5s, tween;
+  h2 {
+    font-size: 32px;
+    font-weight: 550;
+  }
+  p {
+    margin-top: 20px;
+    font-size: 24px;
+  }
 `;
 
 const HomeViewer = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
+  const navigate = useNavigate();
+  const onClickBoard = () => {
+    navigate("/board");
+  };
+  const onClickMovie = () => {
+    navigate("/movie");
+  };
+  const onClickChat = () => {
+    navigate("/chat");
   };
   return (
     <Wrapper>
-      <Slider {...settings}>
-        <SliderItem src={`./asset/image/mbti1.jpg`} />
-        <SliderItem src={`./asset/image/mbti2.jpg`} />
-        <SliderItem src={`./asset/image/mbti3.jpg`} />
-      </Slider>
+      <Grid>
+        <HomeBox onClick={onClickBoard}>
+          <h2>MBTI 게시판에서 소통해보세요! ✨</h2>
+          <p>자신과 같은 MBTI를 가진 사람들과 자유롭게 소통할수 있습니다.</p>
+        </HomeBox>
+        <HomeBox onClick={onClickMovie}>
+          <h2>영화 추천! 🎥</h2>
+          <p>MBTI에 따라 영화를 추천해드립니다.</p>
+        </HomeBox>
+        <HomeBox onClick={onClickChat}>
+          <h2>채팅으로 소통하기! 💬</h2>
+          <p>같은 MBTI를 가진 사람과 대화해보세요.</p>
+        </HomeBox>
+      </Grid>
     </Wrapper>
   );
 };
